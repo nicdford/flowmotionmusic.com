f8026caa27fb004c6e82f6b8a92e95478bbfb425
Nic Ford <nicdford@gmail.com>
2021-08-06 21:30:39 -0700

    Website updates

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,15 @@
 import Head from "next/head";
+import Link from "next/link";
 import Image from "next/image";
+
 import styles from "../styles/Home.module.css";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faFacebook,
+  faInstagram,
+  faSpotify,
+  faApple,
+} from "@fortawesome/free-brands-svg-icons";
 
 export default function Home() {
   return (
@@ -21,32 +30,135 @@ export default function Home() {
         }}
       >
         <h1 className={styles.title}>Flowmotion</h1>
-        <p className={styles.shadows}>
-          Follow us on{" "}
-          <a href="https://www.facebook.com/Flowmotionseattle/">Facebook</a>
-        </p>
+
+        <div className="flex gap-4 mt-8">
+          <Link href="https://www.facebook.com/Flowmotionseattle/">
+            <a className="flex items-center px-4 py-2 text-white transition rounded-sm shadow-md hover:no-underline backdrop-filter backdrop-blur backdrop-contrast-50">
+              <FontAwesomeIcon
+                icon={faFacebook}
+                className="inline-block w-10 pr-2"
+              />{" "}
+              Follow us on Facebook
+            </a>
+          </Link>
+
+          <Link href="https://www.instagram.com/funkyflowmotion/">
+            <a className="flex items-center px-4 py-2 text-white transition rounded-sm shadow-md hover:no-underline backdrop-filter backdrop-blur backdrop-contrast-50">
+              <FontAwesomeIcon
+                icon={faInstagram}
+                className="inline-block w-10 pr-2"
+              />{" "}
+              Follow us on Instagram
+            </a>
+          </Link>
+        </div>
       </main>
 
-      <section className={styles.reader}>
-        <h2>Upcoming Shows</h2>
-        <ul>
-          <li>
-            <h3>Seattle Waterfront</h3>
-            <p>
-              {" "}
-              {`We're`} so excited to announce our first show back in over a
-              year. Join us for a FREE concert on the Seattle waterfront. Big
-              thanks to Downtown Seattle Association for hosting! Dust off them
-              dancing shoes. {`We're`} back!!! Showtime is 5:00pm, all ages
-              welcome!
-            </p>
-            <p>Saturday, August 7th, 2021</p>
-            <p>Pier 62 Downtown</p>
-          </li>
-        </ul>
-      </section>
+      <div className="py-32 space-y-32">
+        <section className="max-w-2xl mx-auto space-y-6">
+          <h2 className="text-5xl">About Flowmotion</h2>
+          <p>
+            Founded with the sole mission to make people move to the music.
+            Flowmotion is a live band experience that pushes all genre
+            boundaries. Their music can harness the inspiration to unite
+            audiences from all walks through the language of music.
+          </p>
+          <p>
+            The group's typical formation starts as a 5 piece group led by
+            singer-guitarist Joshua Clauson. The lineup always features drums, a
+            percussionist extraordinaire, bass, lead guitar, and often times
+            keyboards. Clauson assembles high-caliber musicians to indulge and
+            explore live improv realms through the framework of Flowmotion
+            songs. All songs lead to amoment of free-form expression in some way
+            for all.
+          </p>
+        </section>
 
-      <footer className={styles.footer}>&copy; Flowmotion 2021</footer>
+        <section className="max-w-2xl mx-auto space-y-6">
+          <h2 className="text-5xl">Contact Us</h2>
+          <p>
+            <Link href="mailto:booking@flowmotionmusic.com">
+              <a title="Email us at booking@flowmotionmusic.com">
+                booking@flowmotionmusic.com
+              </a>
+            </Link>
+          </p>
+        </section>
+
+        <section className="max-w-2xl mx-auto space-y-6">
+          <h2 className="mb-8 text-5xl">Upcoming Shows</h2>
+          <ul>
+            <li className="p-8 space-y-2 bg-gray-700 rounded-sm">
+              <h3 className="text-3xl">Seattle Waterfront</h3>
+              <p>
+                {" "}
+                {`We're`} so excited to announce our first show back in over a
+                year. Join us for a FREE concert on the Seattle waterfront. Big
+                thanks to Downtown Seattle Association for hosting! Dust off
+                them dancing shoes. {`We're`} back!!! Showtime is 5:00pm, all
+                ages welcome!
+              </p>
+              <p className="mt-4">
+                <span className="inline-block w-20 font-bold">When:</span>
+                Saturday, August 7th, 2021
+              </p>
+              <p>
+                <span className="inline-block w-20 font-bold">Where:</span>Pier
+                62 Downtown
+              </p>
+            </li>
+          </ul>
+        </section>
+
+        <section className="max-w-2xl mx-auto space-y-6">
+          <h2 className="mb-8 text-5xl">Is That Right</h2>
+          <ul>
+            <li className="flex items-center">
+              <Image
+                src="/is-that-right-cropped.jpg"
+                alt="Albumn art for Is That Right by Flowmotion"
+                width="300"
+                height="300"
+              />
+              <div className="flex flex-col ml-8">
+                <Link href="https://open.spotify.com/album/1ZmrAA9sR5ppqxgMWcPopy">
+                  <a
+                    title="Listen on Spotify"
+                    className="flex items-center my-4"
+                  >
+                    <FontAwesomeIcon
+                      icon={faSpotify}
+                      className="inline-block pr-4 w-14"
+                    />
+                    <h3 className="inline-block text-3xl">Spotify</h3>
+                  </a>
+                </Link>
+                <Link href="https://music.apple.com/us/album/is-that-right/260843815">
+                  <a
+                    title="Listen on Apple Music"
+                    className="flex items-center my-4"
+                  >
+                    <FontAwesomeIcon
+                      icon={faApple}
+                      className="inline-block pr-4 w-14"
+                    />
+                    <h3 className="inline-block text-3xl">Apple Music</h3>
+                  </a>
+                </Link>
+              </div>
+            </li>
+          </ul>
+        </section>
+      </div>
+
+      <footer className="my-8 text-sm">
+        &copy; Flowmotion 2021 |{" "}
+        <Link href="mailto:booking@flowmotionmusic.com">
+          <a title="Email us at booking@flowmotionmusic.com">
+            booking@flowmotionmusic.com
+          </a>
+        </Link>
+      </footer>
     </div>
   );
 }
